49a7aabc8a9ca19ce2e51183e5fb2f8342883b37
Eray Cepni <cepnieray.tr@gmail.com>
2026-08-25 17:20:07 +0300

    added base functions and structs for tcp connect latency

--- bpf/tcp_connect.bpf.c
+++ bpf/tcp_connect.bpf.c
@@ -8,47 +8,47 @@
 #define AF_INET 2
 #define AF_INET6 10
 
+#define TCP_ESTABLISHED 1
+#define TCP_SYN_SENT 2
+#define TCP_CLOSE 7
+#define IPPROTO_TCP 6
+
+
 const volatile u64 target_min_us = 0;
 const volatile pid_t target_tgid = 0;
 
-struct piddata {
+struct connect_start {
     char comm[TASK_COMM_LEN];
     u64 ts;
     u32 tgid;
 };
 
 struct {
-    __uint(type, BPF_MAP_TYPE_HASH);
+    __uint(type, BPF_MAP_TYPE_LRU_HASH);
     __uint(max_entries, 4096);
     __type(key, struct sock *);
-    __type(value, struct piddata);
-} start SEC(".maps");
+    __type(value, struct connect_start);
+} tcp_connect_starts SEC(".maps");
 
 struct {
-    __uint(type, BPF_MAP_TYPE_PERF_EVENT_ARRAY);
-    __uint(key_size, sizeof(u32));
-    __uint(value_size, sizeof(u32));
-} events SEC(".maps");
-
-struct {
-    __uint(type, BPF_MAP_TYPE_HASH);
-    __uint(max_entries, 4096);
-    __type(key, struct sock *);
-    __type(value, u32);
-} completion SEC(".maps");
+    __uint(type, BPF_MAP_TYPE_ARRAY);
+    __uint(max_entries, 7);
+    __type(key, __u32);
+    __type(value, __u64);
+} tcp_connect_latency_buckets SEC(".maps");
 
 static int trace_connect(struct sock *sk)
 {
     u32 tgid = bpf_get_current_pid_tgid() >> 32;
-    struct piddata piddata = {};
+    struct connect_start start = {};
 
     if (target_tgid && tgid != target_tgid)
         return 0;
 
-    bpf_get_current_comm(&piddata.comm, sizeof(piddata.comm));
-    piddata.ts = bpf_ktime_get_ns();
-    piddata.tgid = tgid;
-    bpf_map_update_elem(&start, &sk, &piddata, 0);
+    bpf_get_current_comm(&start.comm, sizeof(start.comm));
+    start.ts = bpf_ktime_get_ns();
+    start.tgid = tgid;
+    bpf_map_update_elem(&tcp_connect_starts, &sk, &start, 0);
     return 0;
 }
 
@@ -58,8 +58,25 @@ int BPF_KPROBE(tcp_v4_connect, struct sock *sk)
     return trace_connect(sk);
 }
 
+SEC("kprobe/tcp_v6_connect")
+int BPF_KPROBE(tcp_v6_connect, struct sock *sk)
+{
+    return trace_connect(sk);
+}
+
+
 SEC("tracepoint/sock/inet_sock_set_state")
 int handle_set_state(struct trace_event_raw_inet_sock_set_state *ctx)
 {
-    
+    struct sock *sk = (struct sock *)ctx->skaddr;
+
+    if (ctx->protocol != IPPROTO_TCP)
+        return 0;
+
+    if (ctx->newstate != TCP_ESTABLISHED)
+        return 0;
+
+    struct connect_start *start = bpf_map_lookup_elem(&tcp_connect_starts, &sk);
+    if (!start)
+        return 0;
 }
